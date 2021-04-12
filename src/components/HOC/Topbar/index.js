27a9fbe3ed8e27188e9e9804411c570fca53c2ec
jhadeepak036 <deepakredmis1@gmail.com>
2021-04-12 23:32:39 +0530

    aniversary commit

--- src/components/HOC/Topbar/index.js
+++ src/components/HOC/Topbar/index.js
@@ -1,29 +1,40 @@
 import { makeStyles } from "@material-ui/core";
 import { Container } from "react-bootstrap";
+import Aniversary from "../../Aniversary";
 
 const useStyles = makeStyles((theme) => ({
   topBarContainer: {
     color: theme.palette.primary.main,
     display: "flex",
-    height: "300px",
+    //height: "300px",
+    minHeight: "300px",
+
     background: `url("/assets/svg/topbar-bg.svg")`,
-    height: "300px",
-    backgroundPosition: "center",
-    ovjectFit: "contain",
+    // backgroundPosition: "center",
+    ovjectFit: "cover",
+    backgroundRepeat: "no-repeat",
   },
   topBarContents: {
-    display: "flex", 
+    display: "flex",
     justifyContent: "center",
     alignItems: "center",
     width: "100%",
     fontSize: "25px",
+  },
+
+  aniversary: {
+    width: "100%",
   },
 }));
 const Topbar = () => {
   const classes = useStyles();
   return (
     <Container fluid="true" className={classes.topBarContainer}>
-      <Container className={classes.topBarContents}>Prince ❤ Afreen</Container>
+      <Container className={classes.topBarContents}>
+        <Container className={classes.aniversary}>
+          <Aniversary />
+        </Container>
+      </Container>
     </Container>
   );
 };
